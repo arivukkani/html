c15986830de1102779ffb7bf0c33b7b2c8c05574
arivukkani <arivukkani2007@gmail.com>
2026-02-25 18:04:23 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,30 @@
+class stud{
+  int sno;
+  String name;
+  int m1;
+  int m2;
+  stud(int a,String b,int m1,int m2){
+    sno=a;
+    name=b;
+    m1=c;
+    m2=d;
+    
+  }
+  void disp(){
+    System.out.println("reg no:"+sno);
+    System.out.println("studentname:"+name);
+    System.out.println("mark1:"+m1);
+    System.out.println("mark2:"+m2);
+  }
+  void cal(){
+    total=m1+m2;
+    System.out.println("total:"+total);
+}
+public class demo{
+  public static void main(String[] args){
+   Scanner sc=new Scanner(System.in);
+   int a=sc.nextInt();
+   String b=sc.nextInt();
+   s1.disp();
+  }
+}
